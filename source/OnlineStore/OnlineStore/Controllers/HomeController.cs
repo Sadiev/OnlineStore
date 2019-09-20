769b9d0a413577ed89e88911a87aacabe0ebaa40
Shod <dilshod.sadiev@outlook.com>
2019-09-20 14:05:16 -0400

    Created MVC

--- source/OnlineStore/OnlineStore/Controllers/HomeController.cs
+++ source/OnlineStore/OnlineStore/Controllers/HomeController.cs
@@ -20,6 +20,11 @@ namespace OnlineStore.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
